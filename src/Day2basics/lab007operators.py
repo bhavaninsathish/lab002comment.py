58ed673e73ee49e55f996548840a3a9f6dba4bf4
Bhavaninbts <bhavaninagaraj95@gmail.com>
2024-10-19 21:09:53 +0530

    python basics2 added

---
+++ src/Day2basics/lab007operators.py
@@ -0,0 +1,42 @@
+# a=100
+# b=200
+# print(a+b)  #addition operation
+
+# 1)arithemetic operators (+-*/)
+# 2) relational operators(<,>,<=,>=)
+# 3)logical operators(True false)
+
+#Arithmetic operators
+# a=5
+# b=2
+# print(a+b)  # addition of a , b
+# print(a-b) # subtraction
+# print(a*b) # multiplication
+# print(a/b)   # 2.5
+# print(a//b)   # 2 quotient
+# print(a%b)  # 1 remainder
+# print(a**b)  # 5**2 = 25
+
+# Relational operators/comparison operators
+# > <  >=  <=  !=  ==
+# Relational operators always returns boolean value True/False
+# a=5
+# b=10
+#
+# print(a>b)  # False
+# print(a<b) # True
+# print(a==b) # False
+# print(a!=b) # True
+# print(a<=b)  # True
+# print(a>=b) # False
+
+#Logical operators
+#  and  or  not
+#always returns boolean value True/False
+a=True
+b=False
+
+print(a and b) # False
+print(a or b) # True
+print(not a) # False
+
